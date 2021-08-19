9c67a0a46ea74663e9eb8110dfcf7f3f80c0e0a9
MuriloMininel <murilomininel1@hotmail.com>
2021-08-19 18:49:57 -0300

    fazendo o css

--- js/lana1.js
+++ js/lana1.js
@@ -1,6 +1,6 @@
 function resposta()
 {
-    var respost = prompt("O que ele irá fazer?")
+    var respost = prompt("O que ela irá fazer?")
     while (respost != 1 && respost != 2){
         alert ("Escolha inválida \n Clique novamente para responder")
         respost = prompt("O que o grupo deve fazer?")
@@ -9,7 +9,7 @@ function resposta()
 
     if (respost == 1) {
         alert ("Lana tenta se aproximar do leão com cuidado porém pisa em um galho seco, que chama atenção do animal que avança nela com grande ferocidade.")
-        
+
         location.href = "gameover.html"
        
     } else {
